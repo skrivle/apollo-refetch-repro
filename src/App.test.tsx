d643eb0109c5b7079dcaa2f2d9f6ee000f6a6bb9
Jelle Versele <jelle.versele@hootsuite.com>
2022-05-30 20:50:21 +0200

    add test that passes with apollo 3.5.x

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,83 @@
-import React from 'react';
+import { ApolloClient, gql, useMutation, useQuery, InMemoryCache, ApolloProvider } from '@apollo/client';
+import { BatchHttpLink } from '@apollo/client/link/batch-http';
+import { setupServer } from 'msw/node';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import userEvent from '@testing-library/user-event';
+import { rest } from 'msw';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+const MUTATION_1 = gql`
+    mutation DoSomething {
+        doSomething {
+            message
+        }
+    }
+`;
+
+const QUERY_1 = gql`
+    query Items {
+        items {
+            id
+        }
+    }
+`;
+
+const Test = () => {
+    const { data } = useQuery(QUERY_1);
+    const [mutate] = useMutation(MUTATION_1, {
+        awaitRefetchQueries: true,
+        refetchQueries: [{ query: QUERY_1 }],
+    });
+
+    const { items = [] } = data || {};
+
+    return (
+        <>
+            <button onClick={() => mutate()} type="button">
+                mutate
+            </button>
+            {items.map((c: any) => (
+                <div key={c.id}>item {c.id}</div>
+            ))}
+        </>
+    );
+};
+
+const server = setupServer();
+
+beforeAll(() => {
+  server.listen();
+});
+
+test('Should execute refetchQueries', async () => {
+
+    const apolloClient = new ApolloClient({
+      link: new BatchHttpLink({
+        uri: '/graphql',
+      }),
+      cache: new InMemoryCache(),
+    });
+
+    let count = 0;
+
+    server.use(
+        rest.post('/graphql', async (req, res, ctx) => {
+            const responses = [
+                [{ data: { items: [{ id: 1 }, { id: 2 }] } }],
+                [{ data: { doSomething: { message: 'success' } } }],
+                [{ data: { items: [{ id: 1 }, { id: 2 }, { id: 3 }] } }],
+            ];
+
+            const response = responses[count++];
+
+            return res(ctx.status(200), ctx.json(response));
+        })
+    );
+
+    render(<ApolloProvider client={apolloClient}><Test /></ApolloProvider>);
+
+    await screen.findByText('item 1');
+
+    userEvent.click(screen.getByRole('button', { name: /mutate/i }));
+
+    await screen.findByText('item 3');
 });
